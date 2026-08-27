37521332c596eff80bccc84a6760edf69a861a00
camorgdatabase <camorgdatabase@gmail.com>
2026-08-27 07:20:41 -0400

    Column is 'nuts_present' not 'latest_nuts'

--- scripts/export_public_data.py
+++ scripts/export_public_data.py
@@ -36,7 +36,7 @@ CAM_TREE_COLUMNS = (
     "latest_height_date",
     "latest_water_date",
     # These fields drive the CAM Trees table column and Nuts Present map theme.
-    "latest_nuts",
+    "nuts_present",
     "latest_nuts_date",
     "longitude",
     "latitude",
